6764af32fc23cc200bda0bb0d801004ee97b7c3c
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2022-06-10 02:23:03 +0100

    Add files via upload

--- GryllsComboSounds.lua
+++ GryllsComboSounds.lua
@@ -8,11 +8,8 @@ local GryllsComboSounds = CreateFrame("Frame", nil, UIParent)
 GryllsComboSounds:RegisterEvent("ADDON_LOADED")
 GryllsComboSounds:RegisterEvent("PLAYER_COMBO_POINTS")
 GryllsComboSounds:SetScript("OnEvent", function()
-    if event == "ADDON_LOADED" then
-        if not GryllsComboSounds.loaded then            
-            DEFAULT_CHAT_FRAME:AddMessage("|cFFFF9900Grylls|rComboSounds loaded!")
-            GryllsComboSounds.loaded = true
-        end
+    if event == "ADDON_LOADED" then        
+        DEFAULT_CHAT_FRAME:AddMessage("|cFFFF9900Grylls|rComboSounds loaded!")
     elseif event == "PLAYER_COMBO_POINTS" then
         GryllsComboSounds_points()
     end
