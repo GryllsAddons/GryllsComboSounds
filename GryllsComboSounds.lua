52466de4adc30daae715dabd6c42cc01c0232233
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2022-06-10 01:34:52 +0100

    Add files via upload

---
+++ GryllsComboSounds.lua
@@ -0,0 +1,19 @@
+local function GryllsComboSounds_points()
+    local media = "Interface\\Addons\\GryllsComboSounds\\media\\"
+    local points = GetComboPoints("player", "target")
+    PlaySoundFile(media..points..".mp3")
+end
+
+local GryllsComboSounds = CreateFrame("Frame", nil, UIParent)
+GryllsComboSounds:RegisterEvent("ADDON_LOADED")
+GryllsComboSounds:RegisterEvent("PLAYER_COMBO_POINTS")
+GryllsComboSounds:SetScript("OnEvent", function()
+    if event == "ADDON_LOADED" then
+        if not GryllsComboSounds.loaded then            
+            DEFAULT_CHAT_FRAME:AddMessage("|cFFFF9900Grylls|rComboSounds loaded!")
+            GryllsComboSounds.loaded = true
+        end
+    elseif event == "PLAYER_COMBO_POINTS" then
+        GryllsComboSounds_points()
+    end
+end)
