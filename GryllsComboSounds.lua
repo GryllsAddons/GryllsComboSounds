61587d2575c112e3afd96dbeb2bcf6341e9e0c01
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-01-31 01:42:28 +0000

    Update GryllsComboSounds.lua

--- GryllsComboSounds.lua
+++ GryllsComboSounds.lua
@@ -1,16 +1,12 @@
-local function GryllsComboSounds_points()
+local function points()
     local media = "Interface\\Addons\\GryllsComboSounds\\media\\"
     local points = GetComboPoints("player", "target")
     PlaySoundFile(media..points..".mp3")
 end
 
-local GryllsComboSounds = CreateFrame("Frame", nil, UIParent)
-GryllsComboSounds:RegisterEvent("ADDON_LOADED")
-GryllsComboSounds:RegisterEvent("PLAYER_COMBO_POINTS")
-GryllsComboSounds:SetScript("OnEvent", function()
-    if event == "ADDON_LOADED" then        
-        DEFAULT_CHAT_FRAME:AddMessage("|cFFFF9900Grylls|rComboSounds loaded!")
-    elseif event == "PLAYER_COMBO_POINTS" then
-        GryllsComboSounds_points()
-    end
+local events = CreateFrame("Frame", nil, UIParent)
+events:RegisterEvent("PLAYER_COMBO_POINTS")
+
+events:SetScript("OnEvent", function()
+    points()
 end)
